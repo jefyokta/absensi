de765e623d4e6b5f6da9c89d9d5ea50fd239b84e
Jepi Oktamipa <jefyokta50@gmail.com>
2024-08-06 08:47:36 +0700

    vite update

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,29 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    build: {
-        outDir: 'public/build',
-        assetsDir: '',
-        manifest: true,
-        rollupOptions: {
-            input: 'resources/js/app.js'
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
         }
+      }
     }
+  },
+  server: {
+    hmr: {
+      host: 'localhost',
+    },
+  },
 });
